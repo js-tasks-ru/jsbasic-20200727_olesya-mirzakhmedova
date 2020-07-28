a0a648feab1f17e6e08c227020a6c2949825865f
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-07-28 13:42:57 +0300

    Вычислить факториал (1-module, 1-task)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  let count = n - 1;
+  let preResult;
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  for (let i = 0; i < n - 1; i++) {
+    preResult = result * count;
+    result = preResult;
+    count--;
+  }
+
+  return result;
 }
