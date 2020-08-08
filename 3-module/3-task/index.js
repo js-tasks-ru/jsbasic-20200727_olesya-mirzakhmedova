ae126ba8c8e2db9a1448df8ca359821a3257618c
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-08-08 19:09:20 +0300

    Функция camelize (3-module, 3-task)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  return camelizedString = str
+    .split('-')
+    .map((word, index) => {
+      if (index > 0) {
+        return word[0].toUpperCase() + word.slice(1);
+      }
+      return word;
+    })
+    .join('');
 }
