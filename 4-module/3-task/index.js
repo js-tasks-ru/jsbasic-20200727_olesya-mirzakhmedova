a74ea0a9347c57334c47cf31cde2599605a605a2
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-08-12 21:04:17 +0300

    Функция highlight (4-module, 3-task)

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,23 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 1; i < table.rows.length; i++) {
+    let currentRow = table.rows[i];
+    let rowClass = currentRow.classList;
+    let isAvailable = currentRow.cells[3].dataset.available;
+    let gender = currentRow.cells[2].textContent;
+    let age = currentRow.cells[1].textContent;
+
+    if(!isAvailable) {
+      currentRow.hidden = true;
+    } else {
+      isAvailable === 'true' ? rowClass.add('available') : rowClass.add('unavailable');
+    }
+
+    gender === 'm' ? rowClass.add('male') : rowClass.add('female');
+
+    if(age < 18) {
+      currentRow.setAttribute('style', 'text-decoration: line-through');
+    }
+  }
 }
