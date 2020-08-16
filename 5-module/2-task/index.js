f6c96a5db78370093d9a5e09405672c62e062e33
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-08-16 22:45:29 +0300

    Скрыть элемент (5-module, 2-task)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let textDiv = document.querySelector(('#text'));
+
+  button.addEventListener('click', (event) => {
+    textDiv.toggleAttribute('hidden');
+  });
 }
