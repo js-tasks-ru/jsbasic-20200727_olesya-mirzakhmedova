6660c2968f93885a7c1bfe2730a6731f3848d8b4
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-08-22 17:11:47 +0300

    Таблица с удаляемыми строками (6-module, 1-task)

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -29,6 +29,52 @@
  * @constructor
  */
 export default class UserTable {
+  get elem() {
+    return this._elem;
+  }
+
   constructor(rows) {
+    this.createTable(rows);
+    this._elem.addEventListener('click', (event) => this.clickDelete(event));
+  }
+
+  createTable(rows) {
+    let table = document.createElement('table');
+    let thead = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>  
+      </thead>
+    `;
+    table.insertAdjacentHTML('afterBegin', thead);
+
+    let tableRows = rows.map(item => {
+      let row = document.createElement('tr');
+      for (let key in item){
+        row.insertAdjacentHTML('beforeend', `<td>${item[key]}</td>`);
+      }
+      row.insertAdjacentHTML('beforeend', `<td><button>X</button></td>`)
+      return row.outerHTML;
+    }).join('');
+    
+    let tbody = `
+      <tbody>
+        ${tableRows}
+      </tbody>
+    `;
+
+    table.insertAdjacentHTML('beforeEnd', tbody);
+    this._elem = table;
+  }
+
+  clickDelete(event) {
+    if(event.target.closest('button')) {
+      event.target.closest('tr').remove();
+    }
   }
 }
