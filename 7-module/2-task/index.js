ac21829790249fd38b008bef82a7a4f9ec0c4ddf
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-08-29 15:08:26 +0300

    Модальное окно (7-module, 2-task)

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,54 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.renderModal();
+    this._modalElem.addEventListener('click', event => this.close(event));
+    document.addEventListener('keydown', event => this.close(event));
+  }
+
+  renderModal() {
+    this._modalElem = document.createElement('div');
+    this._modalElem.classList.add('modal');
+
+    let modalHtml = `
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title"></h3>
+        </div>
+
+        <div class="modal__body"></div>
+      </div>
+    `;
+
+    this._modalElem.innerHTML = modalHtml;
+  }
+
+  open() {
+    document.body.classList.add('is-modal-open');
+    document.body.append(this._modalElem);
+  }
+
+  setTitle(title) {
+    this._modalTitle = this._modalElem.querySelector('.modal__title');
+    this._modalTitle.innerHTML = title;
+  }
+
+  setBody(node) {
+    this._modalBody = this._modalElem.querySelector('.modal__body');
+    this._modalBody.innerHTML = '';
+    this._modalBody.insertAdjacentElement('afterbegin', node);
+  }
+
+  close(event) {
+    if(!event || event.code === 'Escape' || event.target.closest('.modal__close')) {
+      document.body.classList.remove('is-modal-open');
+      this._modalElem.remove();
+    }
   }
 }
