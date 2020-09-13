dd1f7263fe51002fcc00ff469a6b80e9d1fb2301
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2020-09-13 21:47:46 +0300

    Корзина, часть 1 (8-module, 3-task)

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,58 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    let cartItem = this.cartItems.find(item => item.product.id === product.id);
+    
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      let cartItem = {};
+      cartItem.product = product;
+      cartItem.count = 1;
+      this.cartItems.push(cartItem);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find(item => item.product.id === productId);
+    let productAmount = cartItem.count;
+
+    let newAmount = productAmount + amount;
+    if (newAmount === 0) {
+      cartItem.count = newAmount;
+      let index = this.cartItems.indexOf(cartItem);
+      this.cartItems.splice(index, 1);
+    } else {
+      cartItem.count = newAmount;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length ? false : true;
   }
 
   getTotalCount() {
-    // ваш код
+    let productsAmount = 0;
+    
+    for (let product of this.cartItems) {
+      productsAmount += product.count;
+    }
+
+    return productsAmount;
   }
 
   getTotalPrice() {
-    // ваш код
+    let productsPrice = 0;
+    
+    for (let product of this.cartItems) {
+      productsPrice += product.count * product.product.price;
+    }
+
+    return productsPrice;
   }
 
   onProductUpdate() {
